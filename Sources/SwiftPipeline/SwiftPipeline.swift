f206f3d9d2efd59e20e31009d103db376ce83f07
Jacopo <jmangia@me.com>
2018-10-04 22:10:10 -0700

    Splitting the Transformer protocol in specific Mapper and Featurizer protocols

--- Sources/SwiftPipeline/SwiftPipeline.swift
+++ Sources/SwiftPipeline/SwiftPipeline.swift
@@ -24,11 +24,19 @@ protocol PipelineProtocol {
     var metadatas: [String : DataType] { get }
 }
 
-// Transormer interface for implementing Mapper and Featurizer
+// Generic abstract Transormer interface 
 protocol TransformProtocol {
     var name: String { get }
+}
 
-    func transform(pipeline: PipelineProtocol, addOutput: (DataType) -> Void, addFeature: (DataType) -> Void, addMetadata: (String, DataType) -> Void)
+// Specific Transform interface for implementing Mapper
+protocol MapperProtocol : TransformProtocol {
+    func transform(pipeline: PipelineProtocol, addOutput: (DataType) -> Void, addMetadata: (String, DataType) -> Void)
+}
+
+// Specific Transform interface for implementing Featurizer
+protocol FeaturizerProtocol : TransformProtocol {
+    func transform(pipeline: PipelineProtocol, addFeature: (DataType) -> Void, addMetadata: (String, DataType) -> Void)
 }
 
 // Pipeline object to chain and execute several Mappar and Featurizer tranformers
@@ -49,10 +57,16 @@ struct Pipeline : PipelineProtocol {
         inputs.append(input)
         for transformer in transformers {
             print("--- executing \(transformer.name) transformer")
-            transformer.transform(pipeline: self,
-                                  addOutput: { (output) in inputs.append(output) },
-                                  addFeature: { (feature) in features.append(feature) },
-                                  addMetadata: { (name, value) in metadatas[name] = value} )
+            if let transformer = transformer as? MapperProtocol {
+                transformer.transform(pipeline: self,
+                                      addOutput: { (output) in inputs.append(output) },
+                                      addMetadata: { (name, value) in metadatas[name] = value} )
+            }
+            if let transformer = transformer as? FeaturizerProtocol {
+                transformer.transform(pipeline: self,
+                                      addFeature: { (feature) in features.append(feature) },
+                                      addMetadata: { (name, value) in metadatas[name] = value} )
+            }
         }
         
         return features
@@ -61,20 +75,20 @@ struct Pipeline : PipelineProtocol {
 
 
 // Fake Mapper
-struct FakeMapper : TransformProtocol {
+struct FakeMapper : MapperProtocol {
     var name: String
     
-    func transform(pipeline: PipelineProtocol, addOutput: (DataType) -> Void, addFeature: (DataType) -> Void, addMetadata: (String, DataType) -> Void) {
+    func transform(pipeline: PipelineProtocol, addOutput: (DataType) -> Void, addMetadata: (String, DataType) -> Void) {
         addOutput(pipeline.inputs.last!)
     }
 }
 
 
 // Fake Featurizer
-struct FakeFeaturizer : TransformProtocol {
+struct FakeFeaturizer : FeaturizerProtocol {
     var name: String
     
-    func transform(pipeline: PipelineProtocol, addOutput: (DataType) -> Void, addFeature: (DataType) -> Void, addMetadata: (String, DataType) -> Void) {
+    func transform(pipeline: PipelineProtocol, addFeature: (DataType) -> Void, addMetadata: (String, DataType) -> Void) {
         addFeature(pipeline.inputs.last!)
     }
 }
